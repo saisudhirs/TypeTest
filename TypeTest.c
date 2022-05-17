045893b8cf46f2f4f8fb912bc54891e7b51995d9
Sai Sudhir <saisudhirs@outlook.com>
2022-05-17 21:00:16 +0530

    Variables properly named and defined
    WIP: Pretty printing, displaying stats properly

--- TypeTest.c
+++ TypeTest.c
@@ -4,29 +4,41 @@
 
 int main() {
     char key[] = "Hello World!";
-    char i[1000];
-    int l, score = 0;
+    char input[1000];
+    int l1, l2, score = 0;
 
     printf("%s\n", key);
 
     time_t seconds1;
     seconds1 = time(NULL);
 
-    scanf("%[^\n]s", i);
+    scanf("%[^\n]s", input);
 
     time_t seconds2;
     seconds2 = time(NULL);
 
-    l = strlen(i);
-    for(int j = 0; j < l; j++){
-        if(i[j] == key[j]) {
+    printf("\n\n***\n\n");
+
+    l1 = strlen(key);
+    l2 = strlen(input);
+    for(int j = 0; j < l1; j++){
+        if(input[j] == key[j]) {
             score += 1;
-            printf("%c == %c, Score = %d\n",i[j], key[j], score);
+            printf("%c == %c, Score = %d\n",input[j], key[j], score);
         
         } else {
-            printf("%c != %c\n",i[j], key[j]);
+            printf("%c != %c\n",input[j], key[j]);
         }
         
     }
-    printf("\nYour Score is %d\nTime taken is %d", score, seconds2 - seconds1);
+    int time = seconds2 - seconds1;
+    int errors = l1 - score;
+    float accuracy = ((score) * 100) / l1;
+    float time_min = time / 60.0;
+    float cpm = score / time_min;
+
+    printf("\n\n***\n");
+    printf("\nYour Score is %d/%d\nTime taken is %d seconds\n", score, l1, time );
+
+    printf("\nAccuracy: %.2f%%, CPM: %.2f, Errors: %d", accuracy, cpm, errors);
 }

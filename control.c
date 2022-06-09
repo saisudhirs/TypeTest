702c362538ec4ade070c5b5cf9e1a23a3b5557b9
Sai Sudhir <saisudhirs@outlook.com>
2022-06-09 08:09:11 +0530

    Instant mode added
    Many bugs left to be dealt with in instant mode. Freestyle's components to instant mode yet to be added.
    Freestyle mode's instantaneous time calculations updated.

--- control.c
+++ control.c
@@ -4,6 +4,8 @@
 #include<time.h>
 #include<stdlib.h>
 #include <sys/time.h>
+
+//specific function definitions
 int instant();
 int paragraph();
 int freestyle();
@@ -28,9 +30,9 @@ int main() {
         //case '1':
             //timed();
             //break;
-        //case '2':
-            //instant();
-            //break;
+        case '2':
+            instant();
+            break;
         case '3':
             paragraph();
             break;
@@ -45,7 +47,97 @@ int main() {
 
 }
 
+int instant() {
+     char op1;
+    printf("\nYou are in Instant Mode\n\n");
+    printf("Choose your sub-mode: \n1. Simple\n2. Punctuations\n3. Numbers\n4. Difficult\n");
+    scanf("%d", &op1);
+char key[100];
+    
+ if (op1 == 1)
+           {strcpy(key, "as for is am good not so let be");
+            }
+else if (op1 == 2)
+           {strcpy(key, "He is as far not so, as so! thanks & not so much. Dear, you.");
+            }
+else if (op1 == 3)
+           {strcpy(key, "you have 100 in a cent and 1 in a per cent so if you give 2 in 6 it is 33 per cent");
+            } 
+else if (op1 == 4)
+            {strcpy(key, "This is not the 1st TypeTest in your life, I guess!");
+            }
+else {
+        printf("Wrong input");
 
+}   
+    
+
+        char input[100];
+        int l1, l2, score = 0;
+        l1 = strlen(key);
+
+        printf("%s\n", key);
+
+        long long start, stop;
+        int word_count = 0;
+        
+        start = current_timestamp();
+
+        
+        int z = -1;
+        while(z<l1) {
+            //printf("%c\n", key[z]);
+            
+            int y = z;
+            while(key[y] != ' '){
+                printf("%c", key[y]);
+                y++;    
+            }
+            printf("\n");
+            scanf("%c", &input[z]);
+            
+            if(input[z] == ' '){
+                word_count++;
+            }
+            z++;
+            
+            }
+        
+        stop = current_timestamp();
+
+        printf("\n\n***\n\n");
+
+    
+    
+    l2 = strlen(input);
+    for(int letter = 0; letter < l1; letter++){
+        if(input[letter] == key[letter]) {
+            score += 1;
+            printf("%c == %c, Score = %d\n",input[letter], key[letter], score);
+        
+        } else {
+            printf("%c != %c\n",input[letter], key[letter]);
+        }   
+    }
+    
+    //stats calculations
+    float time = (stop - start)/1000.0;
+    int errors = l1 - score;
+    float accuracy = ((score) * 100) / l1;
+    float time_min = time / 60.0;
+    float cpm = score / time_min;
+    float wpm = word_count / time_min;
+
+
+    //stats output
+    printf("\n\n***\n");
+    printf("\nYour Score is %d/%d\nTime taken is %.3f seconds\n", score, l1, time );
+
+    printf("\nAccuracy: %.2f%%, CPM: %.2f, WPM: %.2f, Errors: %d", accuracy, cpm, wpm, errors);
+
+    return 0;
+
+}
 
 int paragraph() {
     char op1;
@@ -162,7 +254,7 @@ int freestyle() {
 
         //instantaneous speed calc
         l2 = strlen(input);
-        inst_time = (inst_stop - inst_start)/1000.0;
+        inst_time = (inst_stop - start)/1000.0;
         float time_min = inst_time / 60.0;
         float cpm = l2 / time_min;
         float wpm = word_count / time_min;
